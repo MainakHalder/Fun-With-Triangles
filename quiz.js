56713d51900088b5770a400e1fa333621ea3e1ed
MainakHalder <76676017+MainakHalder@users.noreply.github.com>
2022-09-13 15:40:48 +0530

    added - questions

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output");
-const correctAnswers = ["180 degree" , "right angled"];
+const correctAnswers = ["180 degree" , "right angled", "three", "three", "acute"];
 function calculateScore(){
     let score = 0;
     let index = 0;
